e07cf877b425113ef22c551cbd407385e27d3068
olga2102 <70343745+olga2102@users.noreply.github.com>
2021-02-01 21:53:59 +0300

    Исправила названия, вывод значения

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const RangeValidity = function (min,max) {
+const checkRangeValidity = function (min,max) {
   if (min < 0 || max < 0 || max <= min) {
     return false;
   }
@@ -6,21 +6,21 @@ const RangeValidity = function (min,max) {
 }
 
 const getRandomIntInclusive = function (min, max) {
-  if (RangeValidity(min, max)) {
+  if (checkRangeValidity(min, max)) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
-  return false;
+  return -1;
 }
 
 getRandomIntInclusive (1, 5);
 
-const getRandomInt = function (min, max, fix = 2) {
-  if (RangeValidity (min, max)) {
-    const rand =  Math.random() * (max - min + 1) + min;
+const getRandomFloatInclusive = function (min, max, fix = 2) {
+  if (checkRangeValidity (min, max)) {
+    const randomNumber =  Math.random() * (max - min + 1) + min;
 
-    return rand.toFixed(fix);
+    return randomNumber.toFixed(fix);
   }
-  return false;
+  return -1;
 }
 
-getRandomInt (1, 5, 3);
+getRandomFloatInclusive (1, 5, 3);
