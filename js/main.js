68d4cfd60d350c7fd612e05f6fa651e6241b708d
olga2102 <70343745+olga2102@users.noreply.github.com>
2021-02-07 22:04:04 +0300

    Работает выбор случайного элемента массива, но не работает создание уникального массива

--- js/main.js
+++ js/main.js
@@ -1,20 +1,82 @@
-const checkRangeValidity = function (min,max) {
+const totalCount = 10;
+const minAvatarCount = 1;
+const maxAvatarCount = 8;
+const offerTitles = [
+  'Лучшее, что ты видел','Выгодное предложение',
+  'Нормальный вариант',  'Закрой глаза и жми сюда',
+  'Не для слабонервных',
+];
+
+const minPrice = 1000;
+const maxPrice = 100000;
+const offerTypes = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+];
+
+const minRooms = 1;
+const maxRooms = 10;
+const minGuests = 1;
+const maxGuests = 30;
+const offerCheckInTimes = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+
+const offerCheckOutTimes = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+
+const offerFeatures = [
+  'wifi',
+  'dishwasher',
+  'parking', 'washer',
+  'elevator',
+  'conditioner',
+];
+
+const offerDescriptions = [
+  'Идеальное место',
+  'Ой, хорошо',
+  'Норм', 'Ну,если не придираться, пойдет',
+  'Все плохо',
+  'За чтооо',
+];
+
+const offerPhotos = [
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
+];
+
+const xMinLocation = 35.65000;
+
+const xMaxLocation = 35.70000;
+
+const yMinLocation = 139.70000;
+
+const yMaxLocation = 139.80000;
+
+const checkRangeValidity = (min,max) => {
   if (min < 0 || max < 0 || max <= min) {
     return false;
   }
   return true;
 }
 
-const getRandomIntInclusive = function (min, max) {
+const getRandomIntInclusive = (min, max) => {
   if (checkRangeValidity(min, max)) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
   return -1;
 }
 
-getRandomIntInclusive (1, 5);
-
-const getRandomFloatInclusive = function (min, max, fix = 2) {
+const getRandomFloatInclusive = (min, max, fix = 2) => {
   if (checkRangeValidity (min, max)) {
     const randomNumber =  Math.random() * (max - min + 1) + min;
 
@@ -23,4 +85,54 @@ const getRandomFloatInclusive = function (min, max, fix = 2) {
   return -1;
 }
 
-getRandomFloatInclusive (1, 5, 3);
+
+const getRandomArrayElement = (elements) => {
+  const indexElement = getRandomIntInclusive(0, elements.length-1)
+  return elements[indexElement];
+};
+
+const getUniqueArray = (array, length) => {
+  const uniqueArray = [];
+  for (let i=0; i <= length-1; i++) {
+    uniqueArray.push(getRandomArrayElement)
+  }
+  return uniqueArray;
+};
+
+const getLocation = () => {
+  const x = getRandomFloatInclusive (xMinLocation, xMaxLocation, 5);
+  const y = getRandomFloatInclusive (yMinLocation, yMaxLocation, 5)
+  return [x, y]
+};
+
+const getAuthor = () => {
+  return  {avatar: 'img/avatars/user' + 0 + getRandomIntInclusive(minAvatarCount, maxAvatarCount) + '.png'};
+};
+
+const getOffer = () => {
+  return {
+    title: getRandomArrayElement (offerTitles),
+    address: getLocation(),
+    price: getRandomIntInclusive (minPrice, maxPrice),
+    type: getRandomArrayElement (offerTypes),
+    rooms: getRandomIntInclusive (minRooms, maxRooms),
+    guests: getRandomIntInclusive (minGuests, maxGuests),
+    checkin: getRandomArrayElement (offerCheckInTimes),
+    checkout: getRandomArrayElement (offerCheckOutTimes),
+    description: getRandomArrayElement (offerDescriptions),
+    features: getUniqueArray (offerFeatures, 6),
+    photos: getUniqueArray (offerPhotos, 3),
+  }
+}
+
+const getTotalObject = () => {
+  return {
+    author: getAuthor(),
+    offer: getOffer(),
+    location: getLocation(),
+  }
+}
+
+const createSimilarOffers = new Array(totalCount).fill(null).map(() => getTotalObject());
+
+createSimilarOffers();
