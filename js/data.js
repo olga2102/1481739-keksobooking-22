d95c19846594b360f47135bc48144959e2d79b49
olga2102 <70343745+olga2102@users.noreply.github.com>
2021-02-13 22:06:43 +0300

    исправила константы

--- js/data.js
+++ js/data.js
@@ -4,63 +4,22 @@ import {getRandomIntInclusive,
   getUniqueArray} from './util.js';
 
 const TOTAL_COUNT = 10;
+
 const MIN_AVATAR_COUNT = 1;
+
 const MAX_AVATAR_COUNT = 8;
-const offerTitles = [
-  'Лучшее, что ты видел',
-  'Выгодное предложение',
-  'Нормальный вариант',
-  'Закрой глаза и жми сюда',
-  'Не для слабонервных',
-];
 
 const MIN_PRICE = 1000;
+
 const MAX_PRICE = 100000;
-const offerTypes = [
-  'palace',
-  'flat',
-  'house',
-  'bungalow',
-];
 
 const MIN_ROOMS = 1;
+
 const MAX_ROOMS = 10;
+
 const MIN_GUESTS = 1;
+
 const MAX_GUESTS = 30;
-const offerCheckInTimes = [
-  '12:00',
-  '13:00',
-  '14:00',
-];
-
-const offerCheckOutTimes = [
-  '12:00',
-  '13:00',
-  '14:00',
-];
-
-const offerFeatures = [
-  'wifi',
-  'dishwasher',
-  'parking', 'washer',
-  'elevator',
-  'conditioner',
-];
-
-const offerDescriptions = [
-  'Идеальное место',
-  'Ой, хорошо',
-  'Норм',
-  'Ну,если не придираться, пойдет',
-  'Все плохо',
-  'За чтооо',
-];
-
-const offerPhotos = [
-  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
-  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
-  'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
-];
 
 const X_MIN_LOCATION = 35.65000;
 
@@ -69,6 +28,56 @@ const X_MAX_LOCATION = 35.70000;
 const Y_MIN_LOCATION = 139.70000;
 
 const Y_MAX_LOCATION = 139.80000;
+
+const OFFER_TITLES = [
+  'Лучшее, что ты видел',
+  'Выгодное предложение',
+  'Нормальный вариант',
+  'Закрой глаза и жми сюда',
+  'Не для слабонервных',
+];
+
+const OFFER_TYPES = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+];
+
+const OFFER_CHECKIN_TIMES = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+
+const OFFER_CHECKOUT_TIMES = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+
+const OFFER_FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking', 'washer',
+  'elevator',
+  'conditioner',
+];
+
+const OFFER_DESKRIPTIONS = [
+  'Идеальное место',
+  'Ой, хорошо',
+  'Норм',
+  'Ну,если не придираться, пойдет',
+  'Все плохо',
+  'За чтооо',
+];
+
+const OFFER_PHOTOS = [
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
+];
 
 const getLocation = () => {
   const x = getRandomFloatInclusive(X_MIN_LOCATION, X_MAX_LOCATION, 5);
@@ -85,17 +94,17 @@ const getTotalObject = () => {
   return {
     author: getAuthor(),
     offer: {
-      title: getRandomArrayElement(offerTitles),
+      title: getRandomArrayElement(OFFER_TITLES),
       address: getLocation(),
       price: getRandomIntInclusive(MIN_PRICE, MAX_PRICE),
-      type: getRandomArrayElement(offerTypes),
+      type: getRandomArrayElement(OFFER_TYPES),
       rooms: getRandomIntInclusive(MIN_ROOMS, MAX_ROOMS),
       guests: getRandomIntInclusive(MIN_GUESTS , MAX_GUESTS ),
-      checkin: getRandomArrayElement(offerCheckInTimes),
-      checkout: getRandomArrayElement(offerCheckOutTimes),
-      description: getRandomArrayElement(offerDescriptions),
-      features: getUniqueArray(offerFeatures),
-      photos: getUniqueArray(offerPhotos),
+      checkin: getRandomArrayElement(OFFER_CHECKIN_TIMES),
+      checkout: getRandomArrayElement(OFFER_CHECKOUT_TIMES),
+      description: getRandomArrayElement(OFFER_DESKRIPTIONS),
+      features: getUniqueArray(OFFER_FEATURES),
+      photos: getUniqueArray(OFFER_PHOTOS),
     },
     location: getLocation(),
   }
--- js/util.js
+++ js/util.js
@@ -47,6 +47,3 @@ export {getRandomIntInclusive,
   getRandomFloatInclusive,
   getRandomArrayElement,
   getUniqueArray};
-
-
-
