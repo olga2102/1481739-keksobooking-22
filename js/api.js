14840ec657c3f3a6e21cca8a2c208150c22cdb00
olga2102 <70343745+olga2102@users.noreply.github.com>
2021-03-08 14:07:00 +0300

    fixes

--- js/api.js
+++ js/api.js
@@ -1,9 +1,5 @@
-// const SERVER_GET_URL = 'https://22.javascript.pages.academy/keksobooking/data';
-// const SERVER_POST_URL = 'https://22.javascript.pages.academy/keksobooking';
-const BASE_URL = 'https://22.javascript.pages.academy/keksobooking';
-const GET_URL = BASE_URL;
-const POST_URL = `${BASE_URL}/data`;
-
+const BASE_URL = 'https://22.javascript.pages.academy/keksobooking'
+const GET_URL = `${BASE_URL}/data`;
 
 const getData = (onSuccess, onFail) => {
   fetch(GET_URL)
@@ -18,7 +14,7 @@ const getData = (onSuccess, onFail) => {
 
 const sendData = (onSuccess, onFail, body) => {
   fetch(
-    POST_URL,
+    BASE_URL,
     {
       method: 'POST',
       body,
